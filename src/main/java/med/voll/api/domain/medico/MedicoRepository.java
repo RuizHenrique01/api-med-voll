a3e8772ec90631636f99d1d9be75f2666af0ff1c
Affonso Ruiz <affonsohenriqueruiz@gmail.com>
2024-07-01 21:16:46 -0400

    feat: Adicionando serviço de agendamento de consultas

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -1,11 +1,34 @@
 package med.voll.api.domain.medico;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface MedicoRepository extends JpaRepository<Medico, Long>{
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
     Page<Medico> findAllByAtivoTrue(Pageable page);
+
+    @Query("""
+            select m from medicos m
+                where
+                m.ativo = true
+                and
+                m.especialidade = :especialidade
+                and
+                m.id not in(
+                        select c.medico.id from consultas c
+                        where
+                        c.data = :data
+                )
+                order by rand()
+                limit 1
+            """)
+    Medico escolherMedicoAleatoriamente(Especialidade especialidade, @NotNull @Future LocalDateTime data);
 
 }
